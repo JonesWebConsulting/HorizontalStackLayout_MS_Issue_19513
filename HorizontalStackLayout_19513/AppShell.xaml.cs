98ed5b1df4c1ba1dc74b3702b46ac6285125a476
Kenneth R. Jones <joneswebconsultingllc@outlook.com>
2023-12-21 07:08:47 -0600

    Add project files.

---
+++ HorizontalStackLayout_19513/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HorizontalStackLayout_19513
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HorizontalStackLayout_19513/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HorizontalStackLayout_19513
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
